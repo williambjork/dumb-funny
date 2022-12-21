9e3dc6b06bf9db9f759acbdd48f58de83b41fa7e
williambjork <william.bjork@gobil.se>
2022-12-21 15:01:49 +0100

    added google login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
+import {auth} from "../utils/firebase"
 
 export default function login() {
+
+    const googleProvider = new GoogleAuthProvider();
+    const GoogleLogin = async () => {
+        try {
+            const result = await signInWithPopup(auth, googleProvider);
+            console.log(result.user)
+        } catch (error) {
+            console.log("error")
+        }
+    }
+
+   
   return (
-    <div>login</div>
+    <div>
+        <div>Login</div>
+        <button onClick={GoogleLogin}>Sign in with Google</button>
+    </div>
   )
 }
 
