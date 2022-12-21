c3c1b4b5483499be84ea27590cbe6e0c22caab73
williambjork <william.bjork@gobil.se>
2022-12-21 11:07:43 +0100

    auth bug fix

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,12 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth"
+import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,5 +16,8 @@ const firebaseConfig = {
   appId: "1:1012770436734:web:9959cf7443ea032eea0a2f"
 };
 
-const auth = getAuth(firebaseApp)
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
+export const auth = getAuth();
+export const db = getFirestore(app);
