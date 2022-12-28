b576dcc8efabf977bc6a00a92dce3f1a670f3265
williambjork <william.bjork@gobil.se>
2022-12-29 00:38:47 +0100

    styling

--- components/Nav/SignUpButton.js
+++ components/Nav/SignUpButton.js
@@ -10,7 +10,8 @@ export default function SignUpButton({url}) {
     } 
   return (
     <div className=''>
-    <button onClick={handleClick} className="border border-black bg-black text-white rounded-sm px-3 py-1 font-bold text-sm ">
+    <button onClick={handleClick} className="hover:bg-teal-500 hover:border-teal-500 
+                                            border border-black bg-black text-white rounded-sm px-3 py-1 font-bold text-sm ">
       SIGN UP
       </button>
 
