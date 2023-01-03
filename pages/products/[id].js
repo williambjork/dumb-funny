cf28d092d01dda24347f8f32273b8fd113c651fd
williambjork <william.bjork@gobil.se>
2023-01-03 13:06:56 +0100

    fetching product

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,6 +1,16 @@
-import React from 'react'
+import { useRouter } from 'next/router';
+import React, { useContext } from 'react'
+import { DumbFunnyContext } from "../../context/DumbFunnyContext";
 
 const Product = () => {
+
+  const { products } = useContext(DumbFunnyContext);
+
+  const router = useRouter();
+  const {productId} = router.query;
+
+  console.log(productId);
+
   return (
     <div>
       <h1>Product!!!</h1>
