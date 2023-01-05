914a585e654c7aeea37123018518e292ba7cb628
williambjork <william.bjork@gobil.se>
2023-01-05 16:37:55 +0100

    debugging

--- pages/products/[productSlug].js
+++ pages/products/[productSlug].js
@@ -1,83 +1,87 @@
-
-import React from 'react'
+import React from "react";
 
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
-const Product = ( { product }) => {
-
+export default function Product({ product }) {
   return (
     <div>
       <h1>Product!!!</h1>
     </div>
-  )
-}
-
-export async function getStaticProps({ params }) {
-  const client = new ApolloClient({
-    uri: 'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clcg4dp6b0q6201t55c5f05qx/master',
-    cache: new InMemoryCache()
-  });
-
-  const data = await client.query({
-    query: gql`
-      query PageProduct($slug: String) {
-        product(where: {slug: $slug}) {
-          id
-          image
-          title
-          price
-          brand
-          description {
-            html
-          }
-        }
-      }
-    `,
-    variables: {
-      slug: params.productSlug
-    }
-  });
-
-  const product = data.data.product;
-
-  return {
-    props: {
-      product
-    }
-  }
+  );
 }
 
 export async function getStaticPaths() {
   const client = new ApolloClient({
-    uri: 'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clcg4dp6b0q6201t55c5f05qx/master',
-    cache: new InMemoryCache()
+    uri: "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clcg4dp6b0q6201t55c5f05qx/master",
+    cache: new InMemoryCache(),
   });
 
   const data = await client.query({
     query: gql`
       query PageProducts {
         products {
-          name
+          brand
           price
           slug
-          image
+          title
         }
       }
-    `
+    `,
   });
 
-  const paths = data.data.products.map(product => {
+  console.log('data', data)
+
+  const paths = data.data.products.map((product) => {
     return {
       params: {
-        productSlug: product.slug
-      }
-    }
-  })
+        productSlug: product.slug,
+      },
+    };
+  });
+
+ 
 
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
-export default Product;
+export async function getStaticProps({params}) {
+  const client = new ApolloClient({
+    uri: "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clcg4dp6b0q6201t55c5f05qx/master",
+    cache: new InMemoryCache(),
+  });
+
+  
+  const data = await client.query({
+    query: gql`
+      query PageProduct($slug: String){
+        product(where: { slug: $slug }) {
+          id
+          
+          name
+          price
+          slug
+          description {
+            html
+          }
+        }
+      }
+    
+    `,
+    variables: {
+      slug: params.productSlug,
+    },
+  });
+
+  console.log("params",);
+
+  const product = data.data.product;
+
+  return {
+    props: {
+      product,
+    },
+  }; 
+}
