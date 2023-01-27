57ec3f8061596e785c774f55d9bd63434fb0de48
williambjork <william.bjork@gobil.se>
2023-01-27 11:50:21 +0100

    inventory coditional size button rendering

--- components/SizeButton.js
+++ components/SizeButton.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React from "react";
 
-export default function SizeButton({size, handleSizeSelection, isSelected}) {
-  
-  if(isSelected) {
-    return (<button onClick={handleSizeSelection} className="border border-emerald-500 bg-emerald-500 text-white  rounded-full p-1 px-3
-    hover:border-emerald-500 h">{size}</button>)
+export default function SizeButton({
+  size,
+  handleSizeSelection,
+  isSelected,
+  outOfStock,
+}) {
+
+  //Checks database if inventory is 0, unselected or selected and renders accordingly
+  if (outOfStock == true) {
+    return (
+      <button
+        onClick={handleSizeSelection}
+        className="border text-gray-300 line-through border-gray-300 rounded-full p-1 px-3
+                             cursor-not-allowed "
+      >
+        {size}
+      </button>
+    );
+  } else {
+    if (isSelected) {
+      return (
+        <button
+          onClick={handleSizeSelection}
+          className="border underline border-emerald-500 bg-emerald-500 text-white  rounded-full p-1 px-3
+    hover:border-emerald-500 h"
+        >
+          {size}
+        </button>
+      );
+    }
+    return (
+      <button
+        onClick={handleSizeSelection}
+        className="border border-black rounded-full p-1 px-3
+                            hover:border-emerald-500 hover:text-emerald-500"
+      >
+        {size}
+      </button>
+    );
   }
-  return (
-    
-    <button onClick={handleSizeSelection} className="border border-black rounded-full p-1 px-3
-                            hover:border-emerald-500 hover:text-emerald-500">{size}</button>
-  )
 }
-
--- pages/products/[productSlug].js
+++ pages/products/[productSlug].js
@@ -10,23 +10,21 @@ const colors = ["pink-500", "red-500", "emerald-500", "cyan-500"];
 
 export default function Product({ product }) {
   const [color, setColor] = useState();
-  const [ selectedSize, setSelectedSize ] = useState()
-  const [selected, setSelected ] = useState(false);
+  const [selectedSize, setSelectedSize] = useState();
+  const [selected, setSelected] = useState(false);
 
   /* useEffect(() => {
     setColor(shuffle(colors).pop());
   }, []); */
 
   function handleSizeSelection(size) {
-    setSelected(!selected)
+    setSelected(!selected);
     setSelectedSize(size);
     console.log(size);
-} 
+  }
 
+  console.log(product.sizes[0].stock.inventory);
 
-
-  console.log(product);
-  
   return (
     <>
       <Head>
@@ -35,18 +33,15 @@ export default function Product({ product }) {
       </Head>
 
       <div className="grid grid-cols-1 md:grid-cols-2 mt-20 ml-20 mr-12">
-
         <p>{product.categories.name}</p>
         <div
           className="col-start-1 justify-center m-auto
                       "
         >
-          <Image  src={product.image.url} height={500} width={500} />
+          <Image src={product.image.url} height={500} width={500} />
         </div>
         <div className="col-start-2 mt-32 ml-12">
-          <h1
-            className={`text-xl font-spacemono font-bold m-auto`}
-          >
+          <h1 className={`text-xl font-spacemono font-bold m-auto`}>
             {product.title}
           </h1>
 
@@ -79,23 +74,53 @@ export default function Product({ product }) {
               data-item-custom2-name="Size"
               data-item-custom2-options="XS|S|M|L|XL|XXL"
               data-item-custom2-value={selectedSize}
-
             >
               Add to cart
             </button>
           </div>
           <div className="border-b border-black mt-8 max-w-xs"></div>
 
-            <div className='flex gap-3 mt-8 font-syne cursor-pointer
-                            '>
-              <SizeButton handleSizeSelection={() => handleSizeSelection("XS")} isSelected={selected} size="XS"/>
-              <SizeButton handleSizeSelection={() => handleSizeSelection("S")} isSelected={selected} size="S"/>
-              <SizeButton handleSizeSelection={() => handleSizeSelection("M")} isSelected={selected} size="M"/>
-              <SizeButton handleSizeSelection={() => handleSizeSelection("L")} isSelected={selected} size="L"/>
-              <SizeButton handleSizeSelection={() => handleSizeSelection("XL")} isSelected={selected} size="XL"/>
-              <SizeButton handleSizeSelection={() => handleSizeSelection("XXL")} isSelected={selected} size="XXL"/>
-              
-            </div>
+          <div
+            className="flex gap-3 mt-8 font-syne cursor-pointer
+                            "
+          >
+            <SizeButton
+              handleSizeSelection={() => handleSizeSelection("XS")}
+              isSelected={selected}
+              outOfStock={product.sizes[0].stock.inventory === 0 ? true : false}
+              size="XS"
+            />
+            <SizeButton
+              handleSizeSelection={() => handleSizeSelection("S")}
+              isSelected={selected}
+              outOfStock={product.sizes[1].stock.inventory === 0 ? true : false}
+              size="S"
+            />
+            <SizeButton
+              handleSizeSelection={() => handleSizeSelection("M")}
+              isSelected={selected}
+              outOfStock={product.sizes[2].stock.inventory === 0 ? true : false}
+              size="M"
+            />
+            <SizeButton
+              handleSizeSelection={() => handleSizeSelection("L")}
+              isSelected={selected}
+              outOfStock={product.sizes[3].stock.inventory === 0 ? true : false}
+              size="L"
+            />
+            <SizeButton
+              handleSizeSelection={() => handleSizeSelection("XL")}
+              isSelected={selected}
+              outOfStock={product.sizes[4].stock.inventory === 0 ? true : false}
+              size="XL"
+            />
+            <SizeButton
+              handleSizeSelection={() => handleSizeSelection("XXL")}
+              isSelected={selected}
+              outOfStock={product.sizes[5].stock.inventory === 0 ? true : false}
+              size="XXL"
+            />
+          </div>
         </div>
       </div>
     </>
@@ -116,7 +141,6 @@ export async function getStaticPaths() {
           slug
           title
           brand
-          
         }
       }
     `,
@@ -165,6 +189,12 @@ export async function getStaticProps({ params }) {
             height
           }
           brand
+          sizes {
+            size
+            stock {
+              inventory
+            }
+          }
         }
       }
     `,
