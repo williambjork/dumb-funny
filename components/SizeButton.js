dcb364388944c975ff87abfdd5cf13b3f002992c
williambjork <william.bjork@gobil.se>
2023-01-31 10:45:35 +0100

    fixed select size bug

--- components/SizeButton.js
+++ components/SizeButton.js
@@ -1,47 +1,36 @@
 import React, { useState } from "react";
 
-export default function SizeButton({ size, handleSizeSelection, outOfStock }) {
-
-  const [selected, setSelected] = useState(false);
-
-  function handleClick() {
-      selected == true ? setSelected(false) : setSelected(true)
-    
-  }
-
-    console.log(selected)
+export default function SizeButton({ size, index, outOfStock, isSelected, handleIndex }) {
   //Checks database if inventory is 0, unselected or selected and renders accordingly
   if (outOfStock == true) {
     return (
       <button
-        onClick={handleSizeSelection}
         className="border text-gray-300 line-through border-gray-300 rounded-full p-1 px-3
-                             cursor-not-allowed "
-      >
+                             cursor-not-allowed ">
         {size}
       </button>
     );
+
   } else {
-    if (selected == true) {
+    if (isSelected == true) {
       return (
         <button
-          onClick={handleClick}
           className="border border-emerald-500 bg-emerald-500 text-white  rounded-full p-1 px-3
-    hover:border-emerald-500 h"
-        >
+                     hover:border-emerald-500 h"
+          onClick={() => handleIndex(index)}>
+          {size}
+        </button>
+      );
+1
+    } else {
+      return (
+        <button
+          className="border border-black rounded-full p-1 px-3
+                     hover:border-emerald-500 hover:text-emerald-500"
+                     onClick={() => handleIndex(index)}>
           {size}
         </button>
       );
     }
-    else {
-    return (
-      <button
-        onClick={handleClick}
-        className="border border-black rounded-full p-1 px-3
-                            hover:border-emerald-500 hover:text-emerald-500"
-      >
-        {size}
-      </button>
-    )};
   }
 }
