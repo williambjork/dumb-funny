f1742b61ecf123ad9162ef99147a6aa695219c08
williambjork <william.bjork@gobil.se>
2023-02-16 11:26:50 +0100

    updated imageoverlay size ang bg

--- components/ImageOverlay.js
+++ components/ImageOverlay.js
@@ -4,13 +4,15 @@ import { GrFormClose } from "react-icons/gr"
 
 export default function ImageOverlay({ src }) {
   return (
-    
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-       
-        <Image src={src} width={1500} height={1800} />
-    
-        <GrFormClose className="absolute right-0 top-4 cursor-pointer" size={25} />
+    <div
+      className="absolute inset-0 flex justify-center items-center"
+      style={{ backgroundColor: "rgba(0, 0, 0, 0.3)" }}
+    >
+      <div className="relative">
+        <Image src={src} width={800} height={1000} objectFit="contain" />
+
+        <GrFormClose className="absolute top-4 right-4 cursor-pointer" size={25} />
       </div>
-    
+    </div>
   );
 }
