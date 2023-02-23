71dd93f8f6b24de6a1808767c76001054fb7eca9
williambjork <william.bjork@gobil.se>
2023-02-23 12:45:59 +0100

    mobile nav update

--- components/Nav/HamburgerMenu.js
+++ components/Nav/HamburgerMenu.js
@@ -11,46 +11,47 @@ import { CgMenuCheese } from "react-icons/cg";
 import { VscChromeClose } from "react-icons/vsc";
 
 function HamburgerMenu({ user, loading }) {
-  const [open, setOpen] = useState();
+  const [open, setOpen] = useState(false);
 
   return (
     <>
-      {open ? (
-        <div className="ml-16"
-          onClick={() => {
-            setOpen(!open);
-          }}
-        >
-          <CgMenuCheese size={30} />
-        </div>
-      ) : (
-        <div className="flex-row  blur-screen">
-          <div
-            onClick={() => {
-              setOpen(!open);
-            }}
-          >
-            <VscChromeClose size={25} />
-          </div>
-          <HomeButton url="/" />
-          <StoreButton url="/store" />
-          <ReadButton url="/read" />
-          <WatchButton url="/watch" />
+  {open ? (
+    <div className="ml-16"
+      onClick={() => {
+        setOpen(!open);
+      }}
+    >
+      <CgMenuCheese size={30} />
+    </div>
+  ) : (
+    <div className="blur-screen flex justify-center items-center">
+      <div className="flex-row">
+      <div className="ml-16"
+      onClick={() => {
+        setOpen(!open);
+      }}
+    >
+        <VscChromeClose size={25} />
+        <HomeButton url="/" />
+        <StoreButton url="/store" />
+        <ReadButton url="/read" />
+        <WatchButton url="/watch" />
 
-          {user ? (
+        {user ? (
             <>
-              <MyAccountButton url="/myAccount" />
-            </>
-          ) : (
+            <MyAccountButton url="/myAccount" />
+          </>
+        ) : (
             <>
-              <LoginButton url="/login" />
-
-              <SignUpButton url="/signup" />
-            </>
-          )}
+            <LoginButton url="/login" />
+            <SignUpButton url="/signup" />
+          </>
+        )}
         </div>
-      )}
-    </>
+      </div>
+    </div>
+  )}
+</>
   );
 }
 
