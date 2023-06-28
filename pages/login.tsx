6c771816c572cb413d33ca0d6e534baac0d55254
williambjork <william.bjork@gobil.se>
2023-06-28 13:59:07 +0200

    added google icon

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,36 +1,43 @@
-import React, { useEffect } from 'react'
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
-import {auth} from "../utils/firebase"
-import { useAuthState } from 'react-firebase-hooks/auth';
-import Router, { useRouter } from 'next/router';
+import React, { useEffect } from "react";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Router, { useRouter } from "next/router";
+import { AiFillGoogleCircle } from "react-icons/ai";
 
 export default function login() {
+  const [user, loading] = useAuthState(auth);
+  const router = useRouter();
 
-    const [user, loading] = useAuthState(auth);
-    const router = useRouter();
+  const googleProvider = new GoogleAuthProvider();
 
-    const googleProvider = new GoogleAuthProvider();
-    
-    const GoogleLogin = async () => {
-        try {
-            const result = await signInWithPopup(auth, googleProvider);
-        
-            router.push("/myAccount")
-        } catch (error) {
-            console.log("error")
-        }
+  const GoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+
+      router.push("/myAccount");
+    } catch (error) {
+      console.log("error");
     }
+  };
 
-   useEffect(() => {
-    user && router.push("/myAccount")
-   }), [];
+  useEffect(() => {
+    user && router.push("/myAccount");
+  }),
+    [];
 
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center pb-12'>
-        
-        <button className='font-spacemono border p-3 border-black
-                            hover:border-dashed hover:border-emerald-500 hover:text-emerald-500' onClick={GoogleLogin}>Sign in with Google</button>
+    <div className="flex min-h-screen flex-col items-center justify-center pb-12">
+      <AiFillGoogleCircle className="mb-3" size={30} />
+        <button
+          className="flex font-spacemono border p-3 border-black
+                            hover:border-dashed hover:border-emerald-500 hover:text-emerald-500"
+          onClick={GoogleLogin}
+        >
+          Sign in with Google
+          
+        </button>
+     
     </div>
-  )
+  );
 }
-
