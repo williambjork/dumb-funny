3f2134f891403502762152879a3ab76a6a93621f
williambjork <william.bjork@gobil.se>
2023-08-02 13:03:07 +0200

    added listen page

---
+++ pages/listen.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function listen() {
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center'>Audio Player</div>
+  )
+}
+
+export default listen
