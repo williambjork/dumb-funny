3222aac29ba29bc1c4b781f49c157a11b04df821
williambjork <william.bjork@gobil.se>
2023-08-03 13:53:36 +0200

    sss

--- components/Audio Player/AudioPlayer.js
+++ components/Audio Player/AudioPlayer.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 function AudioPlayer() {
   return (
-    <div>AudioPlayer</div>
+    <div>
+        <audio src="https%3A//api.soundcloud.com/tracks/615648570" ></audio>
+        <button>back 10</button>
+        <button>play/pause</button>
+        <button>forward 10</button>
+    </div>
   )
 }
 
-export default AudioPlayer
+export { AudioPlayer } 
+
--- pages/listen.tsx
+++ pages/listen.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { AudioPlayer } from "../components/Audio Player/AudioPlayer"
 
 function listen() {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center'>Audio Player</div>
+    <div className='flex min-h-screen flex-col items-center justify-center'>
+      <AudioPlayer />
+    </div>
   )
 }
 
